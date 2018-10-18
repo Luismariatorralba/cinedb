6f8330cbf2a3882a2765aaf2e05621c5bb7ed695
luismaria <lmtorralbap@gmail.com>
2018-10-18 10:14:54 +0200

    Luis

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pelisRouter = require('./routes/peliculas');
 
 var app = express();
 
@@ -43,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/peliculas', pelisRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
